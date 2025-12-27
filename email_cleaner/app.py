54c6a1eaee907f206d781a7ef8ec6c673c037e87
SamuSamos <135957904+SamuSamos@users.noreply.github.com>
2025-12-27 15:49:22 +0100

    Update app.py

--- email_cleaner/app.py
+++ email_cleaner/app.py
@@ -179,4 +179,5 @@ def decode(value):
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
